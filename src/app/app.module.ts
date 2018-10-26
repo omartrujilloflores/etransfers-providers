bcb5bca5deba5b46530b2996f8098a80bb0ecbe2
Omar Trujillo <omar_trujillo_91@hotmail.com>
2018-10-25 22:09:51 -0500

    jquery
    Se agrega Jquery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule , rountingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    rountingComponents,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
